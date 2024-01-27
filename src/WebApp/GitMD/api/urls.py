61193c1832e1f9ca3e42aa0a07e4a0ea5d045dc8
crawfoa4 <aaron.crawford4@mail.dcu.ie>
2024-01-27 13:47:27 +0000

    adding unit tests

--- src/WebApp/GitMD/api/urls.py
+++ src/WebApp/GitMD/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import MarkdownFileView, MarkdownFileCreate, MarkdownFileDelete, MarkdownFileDetails, MarkdownFileEdit
 
 urlpatterns = [
-    path('view', MarkdownFileView.as_view()),
-    path('create', MarkdownFileCreate.as_view()),
-    path('delete', MarkdownFileDelete.as_view()),
-    path('edit', MarkdownFileEdit.as_view()),
+    path('view', MarkdownFileView.as_view(), name='markdown_view'),
+    path('create', MarkdownFileCreate.as_view(), name='markdown_create'),
+    path('delete', MarkdownFileDelete.as_view(), name='markdown_delete'),
+    path('edit', MarkdownFileEdit.as_view(), name='markdown_edit'),
     path('<str:code>', MarkdownFileDetails.as_view(), name='markdown_detail'),
 ]
